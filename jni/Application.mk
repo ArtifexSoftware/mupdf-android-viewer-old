212647cf3247572f9eef7b40245a83ec61178810
Sebastian Rasmussen <sebras@gmail.com>
2016-12-30 00:57:00 +0100

    Harmonize Android API level.
    The java code refers to Android 4.1 (Jelly Bean, API level 16),
    so make that the default everywhere.

--- jni/Application.mk
+++ jni/Application.mk
@@ -3,22 +3,22 @@
 # Suppose our base version is X:
 
 # Version X: armeabi
-#APP_PLATFORM=android-8
+#APP_PLATFORM=android-16
 #APP_ABI := armeabi
 
 # Version X+1: armeabi-v7a (Much faster due to the availability of hardware
 # FP, but cannot be run in the emulator).
-APP_PLATFORM=android-8
+APP_PLATFORM=android-16
 APP_ABI := armeabi-v7a
 
-# Version X+2: x86 (Requires android-9, so a change needs to be made in
+# Version X+2: x86 (Requires android-16, so a change needs to be made in
 # AndroidManifest.xml too)
-#APP_PLATFORM=android-9
+#APP_PLATFORM=android-16
 #APP_ABI := x86
 
-# Version X+3: mips (Requires android-9, so a change needs to be made in
+# Version X+3: mips (Requires android-16, so a change needs to be made in
 # AndroidManifest.xml too)
-#APP_PLATFORM=android-9
+#APP_PLATFORM=android-16
 #APP_ABI := mips
 
 ifdef NDK_PROFILER
